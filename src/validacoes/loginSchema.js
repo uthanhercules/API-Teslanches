2031513311d4b39f2d53ed7f376fa34040a98e24
Uthan H <uthan.hercules12@gmail.com>
2021-07-27 06:12:49 -0300

    Schema de login finalizado.

---
+++ src/validacoes/loginSchema.js
@@ -0,0 +1,8 @@
+const yup = require('./yup');
+
+const loginSchema = yup.object().shape({
+  email: yup.string().email().required,
+  senha: yup.string().required,
+});
+
+module.exports = loginSchema;
