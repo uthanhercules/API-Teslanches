eceddede1d5eb7c1c8258bb87e8d20a0029de7ed
Yuendel Farias <yuendel.fb@hotmail.com>
2021-07-27 05:07:21 -0300

    Schema inicial BD

---
+++ schema.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE TESLANCHES;
+
+CREATE TABLE categoria(
+  id SERIAL NOT NULL primary key,
+  nome varchar(30) NOT NULL
+);
+
+INSERT INTO categoria (nome)
+VALUES
+('Diversos'),
+('Lanches'),
+('Carnes'),
+('Massas'),
+('Pizzas'),
+('Japonesa'),
+('Chinesa'),
+('Mexicano'),
+('Brasileira'),
+('Italiana'),
+('Árabe')
+
+CREATE TABLE usuario(
+  id SERIAL NOT NULL primary key,
+  nome varchar(100) NOT NULL,
+  email varchar(100) NOT NULL UNIQUE,
+  senha text NOT NULL
+);
+
+CREATE TABLE restaurante(
+id SERIAL NOT NULL primary key,
+  usuario_id int NOT NULL references usuario(id),
+  nome varchar(50) NOT NULL,
+  descricao varchar(100),
+  categoria_id int NOT NULL references categoria(id),
+  taxa_entrega int NOT NULL DEFAULT(0),
+  tempo_entrega_minutos int NOT NULL DEFAULT(30),
+  valor_minimo_pedido int NOT NULL DEFAULT(0)
+);
+
+CREATE TABLE produto(
+  id SERIAL NOT NULL primary key,
+  restaurante_id int NOT NULL references restaurante(id),
+  nome varchar(50) NOT NULL,
+  descricao varchar(100),
+  preco int NOT NULL,
+  ativo boolean NOT NULL DEFAULT(TRUE),
+  permite_observacoes boolean NOT NULL DEFAULT(FALSE)
+);
