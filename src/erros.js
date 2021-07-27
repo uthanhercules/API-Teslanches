99d2a7904b019e8aeae60982817af4c2a15458a0
Uthan H <uthan.hercules12@gmail.com>
2021-07-27 16:38:09 -0300

    Sistema de Login com token de autenticação finalizado.

--- src/erros.js
+++ src/erros.js
@@ -1,5 +1,6 @@
 const erro = {
   emailJaRegistrado: 'Este e-mail já está sendo utilizado em outro registro.',
+  erroLogin: 'E-mail e/ou senha incorretos. Tente novamente.',
 };
 
 module.exports = { erro };
