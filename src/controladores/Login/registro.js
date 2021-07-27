1d31af421bf1eb0846271bfd68e3d520fe067477
Uthan H <uthan.hercules12@gmail.com>
2021-07-27 16:01:02 -0300

    Sistema de registro finalizado.

--- src/controladores/Login/registro.js
+++ src/controladores/Login/registro.js
@@ -1,10 +1,44 @@
+const { encrypt } = require('../../banco_de_dados/encriptacao');
 const registroSchema = require('../../validacoes/registroSchema');
+const { query } = require('../../banco_de_dados/conexao');
+const { erro } = require('../../erros');
 
 const registrar = async (req, res) => {
+  const {
+    nome, email, senha, restaurante,
+  } = req.body;
+
   try {
     await registroSchema.validate(req.body);
 
-    return res.status(200).json('Validado!');
+    const encontrarRegistroSQL = 'SELECT * FROM usuario WHERE email = $1';
+    const encontrarRegistro = await query(encontrarRegistroSQL, [email]);
+
+    if (encontrarRegistro.rowCount > 0) {
+      return res.status(400).json(erro.emailJaRegistrado);
+    }
+
+    const senhaEncriptada = JSON.stringify(encrypt(senha));
+    const inserirUsuarioSQL = 'INSERT INTO usuario (nome, email, senha) VALUES ($1, $2, $3)';
+    // eslint-disable-next-line no-unused-vars
+    const inserirUsuario = await query(inserirUsuarioSQL, [nome, email, senhaEncriptada]);
+
+    const encontrarRegistroAtual = await query(encontrarRegistroSQL, [email]);
+    const idRegistroAtual = encontrarRegistroAtual.rows[0].id;
+
+    const inserirRestauranteSQL = 'INSERT INTO restaurante (usuario_id, nome, descricao, categoria_id, taxa_entrega, tempo_entrega_minutos, valor_minimo_pedido) VALUES ($1, $2, $3, $4, $5, $6, $7)';
+    // eslint-disable-next-line no-unused-vars
+    const inserirRestaurante = await query(inserirRestauranteSQL, [
+      idRegistroAtual,
+      restaurante.nome,
+      restaurante.descricao,
+      restaurante.idCategoria,
+      restaurante.taxaEntrega,
+      restaurante.tempoEntregaEmMinutos,
+      restaurante.valorMinimoPedido,
+    ]);
+
+    return res.status(200).json('Usuário registrado com sucesso!');
   } catch (error) {
     return res.status(400).json(error.message);
   }
---
+++ src/erros.js
@@ -0,0 +1,5 @@
+const erro = {
+  emailJaRegistrado: 'Este e-mail já está sendo utilizado em outro registro.',
+};
+
+module.exports = { erro };
