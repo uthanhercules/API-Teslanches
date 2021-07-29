d22f9e1f18bbcd39ff99943a9671c3a500126bc6
Yuendel Farias <yuendel.fb@hotmail.com>
2021-07-28 21:48:11 -0300

    Resolução de alguns bugs e adição de camada de proteção

--- src/controladores/produtos/ativarProduto.js
+++ src/controladores/produtos/ativarProduto.js
@@ -1,6 +1,11 @@
+const jwt = require('jsonwebtoken');
 const conexao = require('../../banco_de_dados/conexao');
 
+const jwtSecret = process.env.JWT_SECRET;
+
 const ativarProduto = async (req, res) => {
+  const dadosUsuarios = jwt.verify(req.header('tokenUsuario'), jwtSecret);
+  const { ID } = dadosUsuarios;
   const { idProduto } = req.params;
 
   try {
@@ -11,8 +16,20 @@ const ativarProduto = async (req, res) => {
       res.status(404).json('Produto informado não existe');
     }
 
-    const query2 = 'update produto set ativo = TRUE where id = $1';
-    const produtoAtualizado = await conexao.query(query2, [idProduto]);
+    const query1 = 'select restaurante_id from produto where id = $1';
+    const { rows: restaurante } = await conexao.query(query1, [idProduto]);
+
+    const restauranteId = restaurante[0].restaurante_id;
+
+    const query2 = 'select usuario_id from restaurante where id = $1';
+    const { rows: usuario } = await conexao.query(query2, [restauranteId]);
+
+    if (usuario[0].usuario_id !== ID) {
+      return res.status(404).json('Produto não pertence ao usuario logado');
+    }
+
+    const query3 = 'update produto set ativo = TRUE where id = $1';
+    const produtoAtualizado = await conexao.query(query3, [idProduto]);
 
     if (produtoAtualizado === 0) {
       return res.status(400).json('O produto não foi atualizado');
--- src/controladores/produtos/obterProduto.js
+++ src/controladores/produtos/obterProduto.js
@@ -1,14 +1,30 @@
+const jwt = require('jsonwebtoken');
 const conexao = require('../../banco_de_dados/conexao');
 
+const jwtSecret = process.env.JWT_SECRET;
+
 const obterProduto = async (req, res) => {
+  const dadosUsuarios = jwt.verify(req.header('tokenUsuario'), jwtSecret);
+  const { ID } = dadosUsuarios;
   const { idProduto } = req.params;
 
   try {
-    const query = 'select * from produto where id = $1 ';
-    const { rowCount, rows: produto } = await conexao.query(query, [idProduto]);
+    const query3 = 'select * from produto where id = $1 ';
+    const { rowCount, rows: produto } = await conexao.query(query3, [idProduto]);
 
     if (rowCount === 0) {
       return res.status(404).json('Produto informado não existe');
+    }
+    const query = 'select restaurante_id from produto where id = $1';
+    const { rows: restaurante } = await conexao.query(query, [idProduto]);
+
+    const restauranteId = restaurante[0].restaurante_id;
+
+    const query2 = 'select usuario_id from restaurante where id = $1';
+    const { rows: usuario } = await conexao.query(query2, [restauranteId]);
+
+    if (usuario[0].usuario_id !== ID) {
+      return res.status(404).json('Produto não pertence ao usuario logado');
     }
 
     res.status(200).json(produto[0]);
