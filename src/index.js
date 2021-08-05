daab4b9be8c9eb4a8bff75ff79009a8620c92c14
Tarcisio Wesley <tarcisiowesley@gmail.com>
2021-08-05 20:39:28 -0300

    aumentando tamanho recebido

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 const rotas = require('./rotas');
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(rotas);
 
 app.listen(8000);
